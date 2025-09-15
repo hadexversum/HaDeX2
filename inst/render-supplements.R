f7a6ad204fd7b97b497de600b9be993915f5d96a
michbur <michalburdukiewicz@gmail.com>
2025-09-15 14:20:53 +0200

    changed path in render supplements

--- inst/render-supplements.R
+++ inst/render-supplements.R
@@ -1,2 +1,3 @@
 rmarkdown::render(input = "vignettes/example.Rmd", 
-                  output_format = bookdown::pdf_document2())
+                  output_format = bookdown::pdf_document2(), 
+                  output_dir = "inst")
